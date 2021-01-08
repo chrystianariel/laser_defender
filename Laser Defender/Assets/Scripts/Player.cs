7eb6f61f36709746615e9b81e8ecbb3d6fb706a9
Chrystian Ariel <chrystian.ariel@gmail.com>
2021-01-08 20:27:11 -0300

    Player and Enemy Shoots

--- Laser Defender/Assets/Scripts/Player.cs
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // configuration parameters
+    [Header("Player")]
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
+    [SerializeField] int health = 200;
+
+    [Header("Projectile")]
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 20f;
     [SerializeField] float projectileFiringPeriod = 0.1f;
